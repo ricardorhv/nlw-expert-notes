e6177a8dbdcc04bb300a6210473bc585e296b584
Ricardo Vinciguerra <ricardorhv.dev@gmail.com>
2024-02-10 10:46:16 -0300

    feat: create the project structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['@rocketseat/eslint-config/react'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+}
